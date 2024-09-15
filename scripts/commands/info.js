ada8a791eec44ace84f5830192419d78b138a6ac
ihad01 <maxjihad59@gmail.com>
2024-09-15 12:39:21 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -30,17 +30,17 @@ var callback = () => api.sendMessage({body:`
 --------------------------------------------
 𝐍𝐚𝐦𝐞       : 𝐋𝐢𝐤𝐡𝐨𝐧 𝐗𝐡𝐨𝐰𝐝𝐡𝐮𝐫𝐲
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐃𝐡𝐚𝐤𝐚, 𝐁𝐚𝐧𝐝𝐠𝐥𝐚𝐝𝐞𝐬𝐡
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬:  𝐒𝐮𝐧𝐝𝐨𝐫𝐠𝐚𝐧𝐣, 𝐆𝐚𝐢𝐛𝐚𝐧𝐝𝐡𝐚
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐆𝐚𝐢𝐛𝐚𝐧𝐝𝐡𝐚, 𝐒𝐮𝐧𝐝𝐨𝐫𝐠𝐨𝐧𝐣
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐑𝐨𝐧𝐠𝐩𝐮𝐫,Bangladesh
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 18+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : likhontasin322@gmail.com
+𝐆𝐦𝐚𝐢𝐥       : 𝐥𝐢𝐤𝐡𝐨𝐧𝐭𝐚𝐬𝐢𝐧322@𝐠𝐦𝐚𝐢𝐥.𝐜𝐨𝐦
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801761838316
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/@LIKHONCHOWDHURY
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://https://www.facebook.com/likhonahmed01`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/....@𝐋𝐈𝐊𝐇𝐎𝐍𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/likhonahmed01`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100092747803433/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100090095242819/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
